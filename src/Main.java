165d348bbd7904d6416164923ed2f8330f0ce1b3
ericatkins8 <eric.atkins@hilliardschools.org>
2022-09-15 15:14:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double winterc = 1228.98;
+        double springc = 1440.00;
+        double summerc = 1537.25;
+        double fallc = 1727.27;
+        double yearCost = winterc + springc + summerc + fallc;
+
+        System.out.println("Your yearly maintenence cost is " + yearCost + " dollars");
+    }
+}
